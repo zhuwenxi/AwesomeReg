2157c704f8e6c87655a77b3c165309c296e246c0
zhuwenxi <zhuwenxi8935@gmail.com>
2016-05-25 23:24:28 +0800

    Build the basic skeleton of "AbstractSyntaxTree".

--- src/com/awesome/regexp/AbstractSyntaxTree.java
+++ src/com/awesome/regexp/AbstractSyntaxTree.java
@@ -1,8 +1,39 @@
 package com.awesome.regexp;
 
 public class AbstractSyntaxTree {
+	
+	public enum Operator {
+		ALTER, CONCAT, REPEAT, UNIT
+	}
+	
+	
+	public class TreeNode {
+		public TreeNode leftOperand;
+		public TreeNode rightOperand;
+		public Operator operator;
+		
+		public TreeNode() {
+			
+		}
+		
+		public TreeNode(Operator operator, TreeNode left, TreeNode right) {
+			this.operator = operator;
+			this.leftOperand = left;
+			this.rightOperand = right;
+		}
+	}
+	
+	
+	
+	
+	public TreeNode root;
 
 	public AbstractSyntaxTree(){
 		
+	}
+	
+	public AbstractSyntaxTree(TreeNode root) {
+		this();
+		this.root = root;
 	}
 }
