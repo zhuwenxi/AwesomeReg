b3163e955893e7593615c03936128daa6cb79827
Zhu <wenxi.zhu@intel.com>
2016-10-06 23:38:18 +0800

    Implementing "Statistic.java".

--- src/com/awesome/regexp/Statistic.java
+++ src/com/awesome/regexp/Statistic.java
@@ -1,20 +1,54 @@
 package com.awesome.regexp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Statistic {
 	
-	public static class Tag {
+	public static enum Tag  {
 		
-		public static final String PrepareAutomata = "PrepareAutomata";
-		
-		public static final String PrepareDfa = "PrepareDfa";
-		
-	}
-	
-	public static void start(String tag) {
+		Automata, 
+		DFA,
 		
 	}
 	
-	public static void increase(String tag) {
+	private static Map<Tag, TimeStamp> timeCounter;
+	
+	static {
+		//
+		// Initialize the time counter hash-table, set keys to the tags in enum "Tag", and values to -1. 
+		//
+		timeCounter = new HashMap<>();
 		
+		for (Tag t : Tag.values()) {
+			timeCounter.put(t, null);
+		}
+	}
+	
+	public static void start(Tag tag) {
+		timeCounter.put(tag, new TimeStamp());
+	}
+	
+	public static void increase(Tag tag, int time) {
+		TimeStamp ts = timeCounter.get(tag);
+		ts.update(time);
+	}
+}
+
+class TimeStamp {
+	int start;
+	int end;
+	
+	public TimeStamp() {
+		this(0, 0);
+	}
+	
+	public TimeStamp(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	public void update(int end) {
+		this.end = end;
 	}
 }
